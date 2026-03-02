ab4fec8421bcc964de4fa1434bf51fd9b5a27b12
Aditya Shanker <aditya.shanker@coforge.com>
2026-03-02 21:22:26 +0530

    css updaated to tailwind

--- shope/product-service/src/main/java/com/shoppe/product/ProductServiceApplication.java
+++ shope/product-service/src/main/java/com/shoppe/product/ProductServiceApplication.java
@@ -10,6 +10,19 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProductServiceApplication {
+    @org.springframework.beans.factory.annotation.Autowired
+    private org.springframework.jdbc.core.JdbcTemplate jdbcTemplate;
+
+    @jakarta.annotation.PostConstruct
+    public void updateDb() {
+        try {
+            jdbcTemplate.execute("ALTER TABLE products MODIFY COLUMN image_url MEDIUMTEXT");
+            System.out.println("SUCCESS: ALTERED products.image_url TO MEDIUMTEXT");
+        } catch (Exception e) {
+            System.err.println("DB ALTER FAILED: " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
